6dfd729cadd74c0cf4f5cad9de9c8591bbd7c1f5
saidAbdirahman <saidqaaje11@gmail.com>
2024-06-30 22:06:07 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>{{title}}</h1>
+<Modal/>
+<Modal/>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name: 'App',
+
+  components:{
+    Modal
+  },
+
+  data(){
+    return {
+      title:'My First Vue App'
+    }
+  },
+  methods:{
+    handleEvent(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+    }
+  }
+    
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
